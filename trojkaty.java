3775a8a9e3237289301b0b4e4adce0b1aa628f21
adriangut <gut.adrian.95@gmail.com>
2022-05-29 14:36:29 +0200

    Weryfikacja wejścia: warunek istnienia trójkąta.

--- trojkaty.java
+++ trojkaty.java
@@ -24,6 +24,18 @@ class Trojkaty {
         System.out.println("Program do rozpoznawania rodzaju trójkąta");
         System.out.println("Uruchom z trzema argumentami liczbowymi - długość boków trójkąta");
     }
+    /**
+    * Funkcja do sprawdzenia czy trójkąt o podanych bokach może zostać zbudowany.
+    * @param {float} a - Długość pierwszego boku.
+    * @param {float} b - Długość drugiego boku.
+    * @param {float} c - Długość trzeciego boku.
+    */
+    public static boolean czyIstniejeTrojkat(float a, float b, float c){
+        if (a + b <= c) return false;
+        if (a + c <= b) return false;
+        if (b + c <= a) return false;
+        return true;
+    }
     /** Glowna funkcja */
     public static void main(String... args) {
         if (args.length != 3) {
@@ -34,6 +46,10 @@ class Trojkaty {
         float b = Float.valueOf(args[1]);
         float c = Float.valueOf(args[2]);
 
+        if (!czyIstniejeTrojkat(a, b, c)) {
+            System.out.println("Z podanych długości boków nie da się zbudować trójkąta");
+            System.exit(3);
+        }
         if (a < 0 || b < 0 || c < 0) {
             System.out.println("Długości boków trójkąta muszą być nieujemne!");
             System.exit(2);
